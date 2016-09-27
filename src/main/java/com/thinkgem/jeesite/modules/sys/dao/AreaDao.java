ed1fce1dfc60d5346a18866c512a03c539fd1109
javaxiaoliu <liuyang_java@126.com>
2016-09-27 09:37:44 +0800

    本地测试commit

--- src/main/java/com/thinkgem/jeesite/modules/sys/dao/AreaDao.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/dao/AreaDao.java
@@ -10,7 +10,7 @@ import com.thinkgem.jeesite.modules.sys.entity.Area;
 /**
  * 区域DAO接口
  * @author ThinkGem
- * @version 2014-05-16
+ * @version 2014-05-16 
  */
 @MyBatisDao
 public interface AreaDao extends TreeDao<Area> {
