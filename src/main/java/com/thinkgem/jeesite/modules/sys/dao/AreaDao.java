ca0749f5da8defe40beb6f64a4ef180f04853e4c
javaxiaoliu <liuyang_java@126.com>
2016-09-27 10:50:01 +0800

    commit 2

--- src/main/java/com/thinkgem/jeesite/modules/sys/dao/AreaDao.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/dao/AreaDao.java
@@ -3,7 +3,7 @@
  */
 package com.thinkgem.jeesite.modules.sys.dao;
 
-import com.thinkgem.jeesite.common.persistence.TreeDao;
+import com.thinkgem.jeesite.common.persistence.TreeDao;  
 import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
 import com.thinkgem.jeesite.modules.sys.entity.Area;
 
